503675dc182f19624e63cfd0a671e6ed363b62a8
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-11-27 13:01:25 +0200

    Add TicketApiController

--- 03. Workshop - Web API and Security/CSharpWeb_CinemaApp_Sept2024/CinemaApp.WebAPI/Controllers/TicketApiController.cs
+++ 03. Workshop - Web API and Security/CSharpWeb_CinemaApp_Sept2024/CinemaApp.WebAPI/Controllers/TicketApiController.cs
@@ -1,13 +1,88 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CinemaApp.Services.Data.Interfaces;
+using CinemaApp.Web.ViewModels.Cinema;
+using CinemaApp.Web.ViewModels.CinemaMovie;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CinemaApp.WebAPI.Controllers
 {
     [ApiController]
-    [Authorize]
-    [Route("[controller]/[action]")]
+    [Route("[controller]")]
     public class TicketApiController : ControllerBase
     {
+        private readonly ICinemaService cinemaService;
+        private readonly ITicketService ticketService;
+        private readonly IMovieService movieService;
 
+        public TicketApiController(ICinemaService cinemaService,
+            ITicketService ticketService, IMovieService movieService)
+        {
+            this.cinemaService = cinemaService;
+            this.ticketService = ticketService;
+            this.movieService = movieService;
+        }
+
+        [HttpGet("[action]/{id?}")]
+        //[ManagerOnly]
+        [ProducesResponseType(typeof(CinemaDetailsViewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetMoviesByCinema(string? id)
+        {
+            Guid cinemaGuid = Guid.Empty;
+            if (!this.IsGuidValid(id, ref cinemaGuid))
+            {
+                return this.BadRequest();
+            }
+
+            CinemaDetailsViewModel? viewModel = await this.cinemaService
+                .GetCinemaDetailsByIdAsync(cinemaGuid);
+            if (viewModel == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(viewModel);
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateAvailableTickets([FromBody] SetAvailableTicketsViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(ModelState);
+            }
+
+            bool isSuccess = await this.ticketService.SetAvailableTicketsAsync(model);
+            if (!isSuccess)
+            {
+                return this.BadRequest();
+            }
+
+            return this.Ok("Ticket availability updated successfully!");
+        }
+
+        protected bool IsGuidValid(string? id, ref Guid parsedGuid)
+        {
+            // Non-existing parameter in the URL
+            if (String.IsNullOrWhiteSpace(id))
+            {
+                return false;
+            }
+
+            // Invalid parameter in the URL
+            bool isGuidValid = Guid.TryParse(id, out parsedGuid);
+            if (!isGuidValid)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
