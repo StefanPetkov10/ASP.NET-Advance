9f03888895bd99f7a9093c0ef3670d1d15dd25ee
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-11-29 17:56:51 +0200

    Add AdminPanel and functionality

--- 04. Workshop - Users and App Layers/CSharpWeb_CinemaApp_Sept2024/CinemaApp.Web/Program.cs
+++ 04. Workshop - Users and App Layers/CSharpWeb_CinemaApp_Sept2024/CinemaApp.Web/Program.cs
@@ -71,6 +71,10 @@ app.UseAuthorization(); // Second -> What can I do?
 app.SeedAdministrator(adminEmail, adminUsername, adminPassword);
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
